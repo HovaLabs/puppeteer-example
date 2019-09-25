3265246393a9806755134f8d877c4657ee46f4df
matthova <matthova87@gmail.com>
2019-09-24 20:58:56 -0700

    Set api url to https

--- src/Bart.js
+++ src/Bart.js
@@ -7,7 +7,7 @@ export const useBartApi = () => {
   React.useEffect(() => {
     async function getStations() {
       try {
-        const reply = await fetch("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=12th&key=MW9S-E7SL-26DU-VV8V&json=y");
+        const reply = await fetch("https://api.bart.gov/api/etd.aspx?cmd=etd&orig=12th&key=MW9S-E7SL-26DU-VV8V&json=y");
         const data = await reply.json();
         const stationList = _.get(data, 'root.station[0].etd');
         setStationList(stationList);
