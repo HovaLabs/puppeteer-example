789cac3f5cb4488d60b11b17d7d9d1594d0892ed
matthova <matthova87@gmail.com>
2019-09-24 20:44:49 -0700

    Updates

---
+++ precache-manifest.170dfd38b85318afaa013a02186f8c77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3843d3db807512e30e3db8a48ce609f",
+    "url": "/puppeteer-example/index.html"
+  },
+  {
+    "revision": "4b3f843abe770ff11941",
+    "url": "/puppeteer-example/static/css/main.0c907582.chunk.css"
+  },
+  {
+    "revision": "ffc796bf009ee32dd919",
+    "url": "/puppeteer-example/static/js/2.279b85f4.chunk.js"
+  },
+  {
+    "revision": "4b3f843abe770ff11941",
+    "url": "/puppeteer-example/static/js/main.cc7ec7a0.chunk.js"
+  },
+  {
+    "revision": "97ae2a31e967d681505e",
+    "url": "/puppeteer-example/static/js/runtime-main.183251a4.js"
+  }
+]);
