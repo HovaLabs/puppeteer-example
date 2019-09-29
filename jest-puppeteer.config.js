60de340bfc1e996e674d0d207b04ae8a54157296
matthova <matthova87@gmail.com>
2019-09-29 11:08:13 -0700

    Bump the launch timeout

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,7 +2,7 @@ module.exports = {
   server: {
     command: "BROWSER=none react-scripts start",
     port: 3000,
-    launchTimeout: 10000
+    launchTimeout: 20000
   },
   launch: {
     headless: process.env.HEADLESS !== "false"
