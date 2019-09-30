d8eb2f9c941b2a6e6e6ed9ea24b3242e37669ac8
matthova <matthova87@gmail.com>
2019-09-29 17:28:06 -0700

    Add test to navigate site

--- e2e/homepageTest.js
+++ e2e/homepageTest.js
@@ -1,3 +1,5 @@
+import urlParser from "url";
+
 function homePageTest() {
   const url = process.env.TEST_PROD
     ? "https://hovalabs.github.io/puppeteer-example"
@@ -11,6 +13,11 @@ function homePageTest() {
 
     it('should be titled "React App"', async () => {
       await expect(page.title()).resolves.toMatch("React App");
+      await expect(page).toClick("a", {
+        text: "12th St. Oakland City Center"
+      });
+      const currentUrl = urlParser.parse(page.url());
+      expect(currentUrl.path).toEqual("/12TH");
     });
   });
 }
